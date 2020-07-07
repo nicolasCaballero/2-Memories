7dcf02b6de42df736cb6ea7a82961b9c6d514218
nicolasCaballero <niicolas.caballero@gmail.com>
2020-07-07 16:27:55 -0300

    Configuración de rutas de categorías

--- website/src/controllers/gridController.js
+++ website/src/controllers/gridController.js
@@ -1,9 +1,24 @@
 const path = require('path');
 
 let gridController = {
-    'show': (req, res) => {
+    'adventureShow': (req, res) => {
         res.render(path.resolve(__dirname, '../views/grids/gastronomia.ejs'));
-    }
+    },
+    'wellbeingShow': (req, res) => {
+        res.render(path.resolve(__dirname, '../views/grids/gastronomia.ejs'));
+    },
+    'coursesShow': (req, res) => {
+        res.render(path.resolve(__dirname, '../views/grids/gastronomia.ejs'));
+    },
+    'entertainmentShow': (req, res) => {
+        res.render(path.resolve(__dirname, '../views/grids/gastronomia.ejs'));
+    },
+    'escapeShow': (req, res) => {
+        res.render(path.resolve(__dirname, '../views/grids/gastronomia.ejs'));
+    },
+    'gastronomyShow': (req, res) => {
+        res.render(path.resolve(__dirname, '../views/grids/gastronomia.ejs'));
+    },
 };
 
 module.exports = gridController;
--- website/src/routes/gridRoutes.js
+++ website/src/routes/gridRoutes.js
@@ -4,5 +4,10 @@ const path = require('path');
 const mainController = require('../controllers/gridController');
 const gridController = require('../controllers/gridController');
 
-router.get('/gastronomia', gridController.show);
+router.get('/gastronomia', gridController.gastronomyShow);
+router.get('/bienestar', gridController.wellbeingShow);
+router.get('/escapadas', gridController.escapeShow);
+router.get('/entretenimiento', gridController.entertainmentShow);
+router.get('/aventuras', gridController.adventureShow);
+router.get('/cursos', gridController.coursesShow);
 module.exports = router;
