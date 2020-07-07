c13223579e215fcf762ddf8f48da4c3b58defe78
nicolasCaballero <niicolas.caballero@gmail.com>
2020-07-07 18:31:08 -0300

    Fotos agregadas a products.json

--- website/src/controllers/gridController.js
+++ website/src/controllers/gridController.js
@@ -1,23 +1,26 @@
 const path = require('path');
+const fs = require('fs');
+
+const products = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../models/products.json')));
 
 let gridController = {
     'adventureShow': (req, res) => {
-        res.render(path.resolve(__dirname, '../views/grids/aventura.ejs'));
+        res.render(path.resolve(__dirname, '../views/grids/aventura.ejs'),{products});
     },
     'wellbeingShow': (req, res) => {
-        res.render(path.resolve(__dirname, '../views/grids/bienestar.ejs'));
+        res.render(path.resolve(__dirname, '../views/grids/bienestar.ejs'),{products});
     },
     'coursesShow': (req, res) => {
-        res.render(path.resolve(__dirname, '../views/grids/cursos.ejs'));
+        res.render(path.resolve(__dirname, '../views/grids/cursos.ejs'),{products});
     },
     'entertainmentShow': (req, res) => {
-        res.render(path.resolve(__dirname, '../views/grids/entretenimiento.ejs'));
+        res.render(path.resolve(__dirname, '../views/grids/entretenimiento.ejs'),{products});
     },
     'escapeShow': (req, res) => {
-        res.render(path.resolve(__dirname, '../views/grids/escapadas.ejs'));
+        res.render(path.resolve(__dirname, '../views/grids/escapadas.ejs'),{products});
     },
     'gastronomyShow': (req, res) => {
-        res.render(path.resolve(__dirname, '../views/grids/gastronomia.ejs'));
+        res.render(path.resolve(__dirname, '../views/grids/gastronomia.ejs'),{products});
     },
 };
 
