560f13d47a7b3bcc3e79f739b736af6841e02236
nicolasCaballero <niicolas.caballero@gmail.com>
2020-07-08 13:14:37 -0300

    configuracion de detalle dinámico de producto

--- website/src/controllers/productController.js
+++ website/src/controllers/productController.js
@@ -1,8 +1,14 @@
 const path = require('path');
+const fs = require ('fs');
+
+const products  = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../models/products.json')));
 
 let productController = {
-    'detail': (req, res) => {
-        res.render(path.resolve(__dirname, '../views/product/productDetail.ejs'));
+    'show': (req,res) => {
+        let products  = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../models/products.json')));
+        let productId = req.params.sku;
+        const product = products.find(p => p.sku == productId);
+        res.render(path.resolve(__dirname, '../views/product/productDetail.ejs'), {product});
     },
     'add': (req, res) => {
         res.render(path.resolve(__dirname, '../views/cart/cart.ejs'));
--- website/src/routes/productRoutes.js
+++ website/src/routes/productRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require('path');
 const productController = require ('../controllers/productController');
 
-router.get('/detalle', productController.detail);
+router.get('/detalle/:sku', productController.show);
 router.get('/detalle/agregar', productController.add);
 router.get('/detalle/experiencias', productController.experiencies);
 
