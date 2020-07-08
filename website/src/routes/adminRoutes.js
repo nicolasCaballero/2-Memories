6e734e96b120925916e86406d8ab86370e77ce78
nicolasCaballero <niicolas.caballero@gmail.com>
2020-07-07 21:11:17 -0300

    C

--- website/src/routes/adminRoutes.js
+++ website/src/routes/adminRoutes.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const multer = require('multer');
 const adminController = require('../controllers/adminController');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.resolve(__dirname, '../../public/img/packs'));
+    },
+    filename: function (req, file, cb) {
+      cb(null, 'plato' + '-' + Date.now()+ path.extname(file.originalname));
+    }
+  })
+   
+const upload= multer({ storage })
 
 router.get('/admin', adminController.index);
 router.get('/admin/login', adminController.login);
