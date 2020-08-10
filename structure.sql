8036add58766be9f9beae9b7179aaca5fa4489fd
Seba Navarro <sebastian.navarro87@gmail.com>
2020-08-10 12:26:50 -0300

    ajustes diagrama y structure.sql en formato para sequelize y nueva table cartProduct

--- structure.sql
+++ structure.sql
@@ -1,98 +1,291 @@
-CREATE SCHEMA `memories_db` ;
-CREATE TABLE `memories_db`.`categories` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
-  `image` TEXT NULL,
-  `visibility` TINYINT NULL,
-  `product_sku` int(11) DEFAULT NULL,
-  `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-  `update_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+--
+-- Host: localhost    Database: memories_db
+-- ------------------------------------------------------
+-- Server version	5.7.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `adminUser`
+--
+
+DROP TABLE IF EXISTS `adminUser`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `adminUser` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `password` text,
+  `role` int(11) DEFAULT NULL,
+  `photo` longtext,
+  `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `deletedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `product_sku_foreign_idx` (`product_sku`),
-  CONSTRAINT `product_sku_foreign` FOREIGN KEY (`product_sku`) REFERENCES `memories_db`.`products` (`sku`) ON DELETE NO ACTION ON UPDATE NO ACTION);
-CREATE TABLE `memories_db`.`products` (
-  `sku` INT NOT NULL,
-  `name` VARCHAR(45) NULL,
-  `description` VARCHAR(100) NULL,
-  `image` TEXT NULL,
-  `visibility` TINYINT NULL,
-  `price` INT NULL,
-  `special_price` INT NULL,
-  `qty` INT NULL,
-  `category_id` INT NULL,
-  `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-  `update_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE KEY `username_UNIQUE` (`username`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `adminUser`
+--
+
+LOCK TABLES `adminUser` WRITE;
+/*!40000 ALTER TABLE `adminUser` DISABLE KEYS */;
+/*!40000 ALTER TABLE `adminUser` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cart`
+--
+
+DROP TABLE IF EXISTS `cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) DEFAULT NULL,
+  `productSku` int(11) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL,
+  `orderNumber` int(11) DEFAULT NULL,
+  `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `deletedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `user_id_foreign_idx` (`userId`),
+  KEY `product_sku_foreign_idx` (`productSku`),
+  CONSTRAINT `product_sku_foreign_cart` FOREIGN KEY (`productSku`) REFERENCES `products` (`sku`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `user_id_foreign_cart` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cart`
+--
+
+LOCK TABLES `cart` WRITE;
+/*!40000 ALTER TABLE `cart` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cart` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cartProduct`
+--
+
+DROP TABLE IF EXISTS `cartProduct`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cartProduct` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `productSku` int(11) DEFAULT NULL,
+  `state` varchar(45) DEFAULT NULL,
+  `salePrice` int(11) DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL,
+  `subTotal` int(11) DEFAULT NULL,
+  `userId` int(11) DEFAULT NULL,
+  `cartId` int(11) DEFAULT NULL,
+  `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `deletedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `userId_foreign_cartProduct_idx` (`userId`),
+  KEY `cartId_foreign_cartProduct_idx` (`cartId`),
+  KEY `productSku_foreign_cartProduct_idx` (`productSku`),
+  CONSTRAINT `cartId_foreign_cartProduct` FOREIGN KEY (`cartId`) REFERENCES `cart` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `productSku_foreign_cartProduct` FOREIGN KEY (`productSku`) REFERENCES `products` (`sku`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `userId_foreign_cartProduct` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cartProduct`
+--
+
+LOCK TABLES `cartProduct` WRITE;
+/*!40000 ALTER TABLE `cartProduct` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cartProduct` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `image` text,
+  `visibility` tinyint(4) DEFAULT NULL,
+  `productSku` int(11) DEFAULT NULL,
+  `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `deletedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `product_sku_foreign_idx` (`productSku`),
+  CONSTRAINT `product_sku_foreign` FOREIGN KEY (`productSku`) REFERENCES `products` (`sku`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `experiences`
+--
+
+DROP TABLE IF EXISTS `experiences`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `experiences` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `description` longtext NOT NULL,
+  `image` longtext NOT NULL,
+  `include` longtext NOT NULL,
+  `website` varchar(50) NOT NULL,
+  `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `deletedAt` varchar(45) DEFAULT 'CURRENT_TIMESTAMP',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `experiences`
+--
+
+LOCK TABLES `experiences` WRITE;
+/*!40000 ALTER TABLE `experiences` DISABLE KEYS */;
+/*!40000 ALTER TABLE `experiences` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
+  `sku` int(11) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `image` text,
+  `visibility` tinyint(4) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `specialPrice` int(11) DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL,
+  `categoryId` int(11) DEFAULT NULL,
+  `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `deletedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`sku`),
-  INDEX `category_id_idx` (`category_id` ASC),
-  CONSTRAINT `category_id_foreign`
-    FOREIGN KEY (`category_id`)
-    REFERENCES `memories_db`.`categories` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-CREATE TABLE `memories_db`.`experiences` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
-  `description` LONGTEXT NULL,
-  `image` LONGTEXT NULL,
-  `include` LONGTEXT NULL,
-  `website` VARCHAR(50) NULL,
-  `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-  `update_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`));
-CREATE TABLE `memories_db`.`adminUser` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
-  `username` VARCHAR(20) NULL,
-  `email` VARCHAR(50) NOT NULL,
-  `password` TEXT NULL,
-  `role` INT NULL,
-  `photo` LONGTEXT NULL,
-  `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-  `update_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  KEY `category_id_idx` (`categoryId`),
+  CONSTRAINT `categoryId_foreign_products` FOREIGN KEY (`categoryId`) REFERENCES `categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transactions`
+--
+
+DROP TABLE IF EXISTS `transactions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `transactions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cart_id` int(11) DEFAULT NULL,
+  `date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `totalAmount` int(11) DEFAULT NULL,
+  `paymentMethod` tinyint(4) DEFAULT NULL,
+  `shippingMethod` tinyint(4) DEFAULT NULL,
+  `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `deletedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC),
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC));
-CREATE TABLE `memories_db`.`users` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
-  `lastName` VARCHAR(45) NULL,
-  `email` VARCHAR(50) NOT NULL,
-  `password` TEXT NULL,
-  `photo` TEXT NULL,
-  `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-  `update_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  KEY `cart_id_foreign_idx` (`cart_id`),
+  CONSTRAINT `cart_id_foreign` FOREIGN KEY (`cart_id`) REFERENCES `cart` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transactions`
+--
+
+LOCK TABLES `transactions` WRITE;
+/*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `lastName` varchar(45) DEFAULT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` text,
+  `photo` text,
+  `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `deletedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC));
-CREATE TABLE `memories_db`.`cart` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `user_id` INT NULL,
-  `product_sku` INT NULL,
-  `price` INT NULL,
-  `qty` INT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `user_id_foreign_idx` (`user_id` ASC),
-  INDEX `product_sku_foreign_idx` (`product_sku` ASC),
-  CONSTRAINT `user_id_foreign_cart`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `memories_db`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `product_sku_foreign_cart`
-    FOREIGN KEY (`product_sku`)
-    REFERENCES `memories_db`.`products` (`sku`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-CREATE TABLE `memories_db`.`transactions` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `cart_id` INT NULL,
-  `date` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
-  `totalAmount` INT NULL,
-  `paymentMethod` TINYINT NULL,
-  `shippingMethod` TINYINT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `cart_id_foreign_idx` (`cart_id` ASC),
-  CONSTRAINT `cart_id_foreign`
-    FOREIGN KEY (`cart_id`)
-    REFERENCES `memories_db`.`cart` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-08-10 12:23:41
