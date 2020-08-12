89b019bb8e53838995d08c848b91233dfa875777
nicolasCaballero <niicolas.caballero@gmail.com>
2020-08-12 20:17:46 -0300

    agrego modelo de experiences

---
+++ website/src/db/models/Experience.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, dataTypes) => {
+    const Experience = sequelize.define('experiences', {
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        name: {
+            type: dataTypes.STRING
+        },
+        description: {
+            type: dataTypes.STRING
+        },
+        image: {
+            type: dataTypes.STRING
+        },
+        include: {
+            type: dataTypes.STRING
+        },
+        website: {
+            type: dataTypes.STRING
+        },
+        productSku: {
+            type: dataTypes.INTEGER
+        }
+    }, {
+        tableName: 'experiences'
+    });
+    return Experience
+};
