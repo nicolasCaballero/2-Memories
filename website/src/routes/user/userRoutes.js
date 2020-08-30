f390d3bc1132fa880d7ac07b8ccf8c8d5380ab22
nicolasCaballero <niicolas.caballero@gmail.com>
2020-08-30 10:56:23 -0300

    agrego middlewares de validaciones de creacion de cuenta y login

--- website/src/routes/user/userRoutes.js
+++ website/src/routes/user/userRoutes.js
@@ -2,103 +2,23 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const multer = require('multer');
-const bcrypt = require('bcrypt');
 const userController = require('../../controllers/userController');
 const loggedInMiddleware = require('../../middlewares/loggedInMiddleware');
 const accountValidationMiddleware = require('../../middlewares/accountValidationMiddleware');
 const notLoggedInMiddleware = require('../../middlewares/notLoggedInMiddleware');
-const db = require('../../db/models');
-
-
-const {
-    check,
-    validationResult,
-    body
-} = require('express-validator');
+const processLoginMiddleware = require('../../middlewares/accountMiddlewares/processLogin');
+const accountCreationMiddleware = require('../../middlewares/accountMiddlewares/accountCreationMiddleware');
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, path.resolve(__dirname, '../../../public/img/users'));
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    }
-})
-
-const upload = multer({
-    storage
+    destination: function (req, file, cb) {cb(null, path.resolve(__dirname, '../../../public/img/users'));},
+    filename: function (req, file, cb) {cb(null, file.originalname);}
 });
+const upload = multer({storage});
 
 router.get('/login', userController.login);
-db.users.findAll()
-    .then((users) => {
-        router.post('/login', [
-            body('email').custom((value) => {
-                for (let i = 0; i < users.length; i++) {
-                    if (users[i].email == value) {
-                        return true
-                    }
-                }
-                return false
-            }).withMessage('Usuario inexistente'),
-            body('password').custom((value, {
-                req
-            }) => {
-                for (let i = 0; i < users.length; i++) {
-                    if (users[i].email == req.body.email) {
-                        if (bcrypt.compareSync(value, users[i].password)) {
-                            return true;
-                        } else {
-                            return false;
-                        }
-                    };
-                };
-            }).withMessage('Contraseña incorrecta'),
-        ], userController.processLogin);
-    });
+router.post('/login', processLoginMiddleware, userController.processLogin);
 router.get('/registro', loggedInMiddleware, userController.register);
-db.users.findAll()
-    .then((users) => {
-        router.post('/registro', upload.single('image'), [
-            check('name').isAlpha().withMessage('El nombre solo debe contener letras de la A-Z'),
-            check('name').isLength({
-                min: 1
-            }).withMessage('El nombre no puede estar vacío'),
-            check('lastName').isAlpha().withMessage('El apellido solo debe contener letras de la A-Z'),
-            check('lastName').isLength({
-                min: 1
-            }).withMessage('El apellido no puede estar vacío'),
-            check('password', 'La contraseña debe contener al ménos 6 caracteres, una minúscula, un número y una mayúscula').matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{6,}$/, 'i'),
-            body('email').custom(value => {
-                let counter = 0;
-                for (let i = 0; i < users.length; i++) {
-                    if (users[i].email == value) {
-                        counter++;
-                    }
-                }
-                if (counter > 0) {
-                    return false;
-                } else {
-                    return true;
-                }
-            }).withMessage('El email ya se encuentra registrado'),
-            body('photo').custom((value, {
-                req
-            }) => {
-                let ext
-                if (req.file = !undefined) {
-                    return true;
-                } else {
-                    ext = '' + path.extname(req.files[0].filename).toLowerCase();
-                }
-                if (ext == '.jpg' || ext == '.jpeg' || ext == '.png' || ext == '.gif') {
-                    return true;
-                }
-                return false;
-            }).withMessage('Solo se aceptan archivos con extensión JPG, JPEG, PNG o GIF')
-
-        ], userController.create)
-    });
+router.post('/registro', accountCreationMiddleware, userController.create);
 router.get('/logout', userController.logout);
 router.get('/mi-cuenta/ver/:id', notLoggedInMiddleware, accountValidationMiddleware, userController.usersShow);
 router.put('/mi-cuenta/ver/:id', upload.single('image'), userController.saveEdit);
