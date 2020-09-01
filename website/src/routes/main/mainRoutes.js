bd044273d4d216503678c648a685fca3fe5268b0
nicolasCaballero <niicolas.caballero@gmail.com>
2020-09-01 13:11:08 -0300

    validación de usuario logueado para ingresar a /abrir/memorie

--- website/src/routes/main/mainRoutes.js
+++ website/src/routes/main/mainRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const mainController = require('../../controllers/mainController');
+const notLoggedInMiddleware = require('../../middlewares/notLoggedInMiddleware');
+
 
 router.get('/', mainController.index);
-router.get('/abrir/memorie', mainController.open);
+router.get('/abrir/memorie', notLoggedInMiddleware, mainController.open);
 router.get('/nosotros', mainController.about);
 router.get('/ayuda', mainController.help);
 router.get('/terminos-y-condiciones', mainController.tyc);
