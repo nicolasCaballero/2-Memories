a14c687b74489a11e70ff5153e723c4a54249266
Seba Navarro <sebastian.navarro87@gmail.com>
2020-09-05 16:00:20 -0300

    arreglo header mobile

--- website/src/db/config/config.js
+++ website/src/db/config/config.js
@@ -5,12 +5,8 @@ module.exports = {
     "password": "root",
     "database": "memories_db",
     "host": "127.0.0.1",
-<<<<<<< HEAD
     "dialect": "mysql",
     "port": 8889
-=======
-    "dialect": "mysql"
->>>>>>> SebaLocal
   },
   "test": {
     "username": "root",
