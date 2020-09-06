682fd1eb47dfbbf7467342568b99cbf26d8e1aea
nicolasCaballero <niicolas.caballero@gmail.com>
2020-09-06 20:53:00 -0300

    correciones sweet alert

--- website/public/js/productAddToCart.js
+++ website/public/js/productAddToCart.js
@@ -1,6 +1,12 @@
 window.addEventListener('load', () => {
     let buyButton = document.querySelector('.buyButton');
-    // buyButton.addEventListener('click', () => {
-        swal("Producto agregado al carrito!", "", "success");
-    // });
+    buyButton.addEventListener('click', () => {
+        Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Producto agregado al carrito!',
+            showConfirmButton: false,
+            timer: 1500
+          })
+    })
 });
