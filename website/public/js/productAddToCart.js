73b964859789c3637c7804c5c64177dbfcafc0e5
nicolasCaballero <niicolas.caballero@gmail.com>
2020-09-06 18:25:52 -0300

    sweet alert en acción comprar

---
+++ website/public/js/productAddToCart.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+    let buyButton = document.querySelector('.buyButton');
+    // buyButton.addEventListener('click', () => {
+        swal("Producto agregado al carrito!", "", "success");
+    // });
+});
