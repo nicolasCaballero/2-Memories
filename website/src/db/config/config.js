d47d3ee5e368cab7b545f5ad59eb631cdcc8e4d2
Seba Navarro <sebastian.navarro87@gmail.com>
2020-09-06 20:57:17 -0300

    arreglo fix header home

--- website/src/db/config/config.js
+++ website/src/db/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
 
   "development": {
     "username": "root",
-    "password": "root",
+    "password": null,
     "database": "memories_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
